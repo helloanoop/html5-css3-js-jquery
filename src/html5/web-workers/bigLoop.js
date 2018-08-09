ced5770fab3adf033189bdac8ff7b0652e858984
Anoop M D <anoop.md@sinilabs.co.in>
2018-08-09 15:32:40 +0530

    Canvas Stuff

--- src/html5/web-workers/bigLoop.js
+++ src/html5/web-workers/bigLoop.js
@@ -1,13 +1,17 @@
 onmessage = function(e) {
   console.log(e.data);
-  writeToExcel(e.data);
+  // writeToExcel(e.data);
 };
 
-// for (var i = 0; i <= 1000000000; i += 1) {
-//    var j = i;
-// }
-// postMessage(j);
-function writeToExcel(data) {
-  postMessage("Finished processing")
-  // cpu intensive task
+console.log("Here");
+for (var i = 0; i <= 1000000000; i += 1) {
+   var j = i;
 }
+console.log("Here again");
+postMessage(j);
+
+
+// function writeToExcel(data) {
+//   postMessage("Finished processing")
+//   // cpu intensive task
+// }
